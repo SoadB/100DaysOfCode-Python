293f7c2485a418578ceb23b86d70fdde90f39a3c
SoadB <soadb321@gmail.com>
2019-08-27 07:08:25 +0300

    Test

---
+++ Day_8.py
@@ -0,0 +1,30 @@
+
+# Ex1
+s1 = "  Good Night! "
+print(s1.strip())
+
+print("--------------------------")
+
+# Ex2
+print(len(s1))
+
+print("--------------------------")
+
+# Ex3
+print(s1.strip().lower())
+
+print("--------------------------")
+
+# Ex4
+print(s1.strip().upper())
+
+print("--------------------------")
+
+# Ex5
+s2 = "Hello, World"
+print(s2.replace('H', 'J'))
+
+print("--------------------------")
+
+# Ex6
+print(s2.split(", "))
