874ca12657951dc03546ff1b1cf10528667b0f64
SoadB <soadb321@gmail.com>
2019-08-28 22:12:59 +0300

    Test

---
+++ Day_9.py
@@ -0,0 +1,22 @@
+
+# Ex1
+age = 20
+txt = "My name is Soad, and I am {}"
+print(txt.format(age))
+
+print("------------------------")
+
+# Ex2
+quantity = 5
+itemno = 692
+price = 50.6
+txt2 = "I want {} pieces of item {} for {} dollars. "
+print(txt2.format(quantity, itemno, price))
+
+print("------------------------")
+
+# Ex3
+txt2 = "I want to pay {2} dollars for {0} pieces of item {1}"
+print(txt2.format(quantity, itemno, price))
+
+
