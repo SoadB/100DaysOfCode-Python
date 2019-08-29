00af27b5f891bf5a541d5d97ca4cd0571b0b1e54
SoadB <soadb321@gmail.com>
2019-08-29 17:21:15 +0300

    Test

---
+++ Day_10.py
@@ -0,0 +1,23 @@
+
+# Ex1
+num1 = 10
+num2 = 4
+print(num1 * num2)
+print(num1 % num2)
+
+print("---------------------------")
+
+# Ex2
+num1 /= 2
+print(num1)
+num2 &= 5
+print(num2)
+
+print("---------------------------")
+
+# Ex3
+print(num1 > num2)
+print(num2 == num1)
+
+
+
