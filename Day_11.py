f31ac74ea3938c29ab43935b8547c8437048fb23
SoadB <soadb321@gmail.com>
2019-08-30 08:03:03 +0300

    Test

---
+++ Day_11.py
@@ -0,0 +1,31 @@
+
+# Ex1
+x = 9
+print(x > 10 or x < 15)
+print(not(x > 5 and x < 10))
+
+print("------------------------")
+
+# Ex2
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is y)
+print(z is not x)
+print(x is z)
+
+print("-----------------------")
+
+# Ex3
+print("apple" not in x)
+print("orange" in y)
+print("apple" in z)
+
+print("-----------------------")
+
+# Ex3
+print(1 & 0)
+print(1 | 1)
+
+
+
