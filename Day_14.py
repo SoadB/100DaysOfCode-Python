e9d203fb708ee3d57a152a07d9f216fcf12205a1
SoadB <soadb321@gmail.com>
2019-09-02 10:09:47 +0300

    Lists2

---
+++ Day_14.py
@@ -0,0 +1,22 @@
+
+# Ex1
+charList = ['A', 'B', 'C', 'D']
+print(charList[1:3])
+print("-------------------------")
+# Ex2
+fruitsList = ["apple", "banana", "cherry"]
+if ('apple' and 'banana') in fruitsList:
+	print("Yes")
+print("-------------------------")
+# Ex3
+S = ['بايثون'] * 5
+print(S)
+print("-------------------------")
+# Ex4
+list1 = [9, 7, 5, 1]
+list2 = [2.4, 5.1, 3.8]
+list3 = list1 + list2
+print(list3)
+
+
+
