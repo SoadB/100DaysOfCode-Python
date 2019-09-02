f8dcd2a66f4b76d10dae8320a058e731ef794a25
SoadB <soadb321@gmail.com>
2019-09-02 09:04:51 +0300

    Lists

---
+++ Day_13.py
@@ -0,0 +1,37 @@
+# Ex1 empty list
+S = []
+print(S)
+print("-------------------------")
+# Ex2
+numbers = [1, 2, 3, 4, 5, 6, 7]
+print(numbers)
+print("-------------------------")
+# Ex3
+fruitsList = ["apple", "banana", "cherry"]
+print(fruitsList)
+print("-------------------------")
+# Ex4
+thisList = ["apple", "banana", "cherry", 1, 2, 3]
+print(thisList)
+print("-------------------------")
+# Ex5 float numbers
+floatNumbers = [1.3, 7.34, 2.5, 90.3, 22.4]
+print(floatNumbers)
+print("-------------------------")
+# Ex6
+print("The second item in fruits list is: " + fruitsList[1])
+print("-------------------------")
+# Ex7
+num = [10, 20, 30]
+for x in num:
+	print(x)
+print("-------------------------")
+# Ex8
+thisList[2] = "blackcurrant"
+print(thisList)
+print("-------------------------")
+# Ex9
+del fruitsList[0]
+print(fruitsList)
+
+
