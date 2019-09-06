768bc0aa43b56cea6a56c6a662b0a918673b7771
SoadB <soadb321@gmail.com>
2019-09-06 14:44:50 +0300

    Tuple2

---
+++ Day_17.py
@@ -0,0 +1,39 @@
+
+# Ex1
+fruitsTuple = ("apple", "banana", "cherry")
+if 'apple' in fruitsTuple:
+	print("Yes")
+print("-------------------------")
+
+# Ex2
+tupleOne = ('بايثون',) * 3
+print(tupleOne)
+print("-------------------------")
+
+# Ex3
+tupleTwo = ('A', 'B', 'C')
+tupleThree = tupleOne + tupleTwo
+print(tupleThree)
+print("-------------------------")
+
+# Ex4
+x = (3, 4, 5, 6)
+x = x + (1, 2, 3)
+print(x)
+print("-------------------------")
+
+# Ex5
+print(len(fruitsTuple))
+print("-------------------------")
+
+# Ex6
+thisTuple = tuple((10, 20, 30, 40))
+print(thisTuple)
+print("-------------------------")
+
+# Ex7
+thisList = ['Python'] * 2
+thisTuple = tuple(thisList)
+print(thisTuple)
+
+
