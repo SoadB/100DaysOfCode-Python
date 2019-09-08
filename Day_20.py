a91b9d84425b22a4ef30a034491016fae74bbf82
SoadB <soadb321@gmail.com>
2019-09-08 20:28:40 +0300

    Sets

---
+++ Day_20.py
@@ -0,0 +1,35 @@
+
+# Ex1
+thisSet = {}
+print(thisSet)
+print("-------------------------")
+
+# Ex2
+Set1 = {'apple, orange, banana'}
+print(Set1)
+print("-------------------------")
+
+# Ex3
+Set2 = {1, 2, 1, 'Ahmed', 'Ahmed'}
+print(Set2)
+print("-------------------------")
+
+# Ex4
+for x in Set2:
+	print(x)
+print("-------------------------")
+
+# Ex5
+print('Ahmed' in Set2)
+print("-------------------------")
+
+# Ex6
+Set1.add('cherry')
+print(Set1)
+print("-------------------------")
+
+# Ex7
+Set2.update([20, 30, 'Khaled'])
+print(Set2)
+
+
