a8b78cf96184b88357c90e5af5264aaea57b6c9b
SoadB <soadb321@gmail.com>
2019-09-17 23:53:35 +0300

    for loop

---
+++ Day_29.py
@@ -0,0 +1,35 @@
+
+# Ex1
+fruits = ['apple', 'banana', 'cherry']
+for x in fruits:
+	print(x)
+print("-------------------------")
+
+# Ex2
+for x in 'python':
+	print(x)
+print("-------------------------")
+
+# Ex3
+fruits = ['apple', 'banana', 'cherry']
+for x in fruits:
+	print(x)
+	if x == 'banana':
+		break
+print("-------------------------")
+
+# Ex4
+fruits = ['apple', 'banana', 'cherry']
+for x in fruits:
+	if x == 'banana':
+		break
+	print(x)
+print("-------------------------")
+
+# Ex5
+fruits = ['apple', 'banana', 'cherry']
+for x in fruits:
+	if x == 'banana':
+		continue
+	print(x)
+print("-------------------------")
