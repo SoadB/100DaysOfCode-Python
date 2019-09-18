afc6cf4ad3dcf765775e4f96f510c4abef5dc7ca
SoadB <soadb321@gmail.com>
2019-09-18 21:38:25 +0300

    for loop

---
+++ Day_30.py
@@ -0,0 +1,32 @@
+
+# Ex1
+for x in range(6):
+    print(x)
+print("-------------------------")
+
+# Ex2
+for x in range(2, 6):
+    print(x)
+print("-------------------------")
+
+# Ex3
+for y in range(2, 30, 3):
+    print(y)
+print("-------------------------")
+
+# Ex4
+for x in range(4):
+    print(x)
+else:
+    print("Finally finished")
+print("-------------------------")
+
+# Ex5
+adj = ["red", "big", "tasty"]
+fruit = ['apple', 'banana', 'cherry']
+for x in adj:
+    for y in fruit:
+       print(x, y)
+print("-------------------------")
+
+
