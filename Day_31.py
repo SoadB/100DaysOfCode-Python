12ca27757b1ed8c6d57228554bf3ce48913a3714
SoadB <soadb321@gmail.com>
2019-09-21 21:47:07 +0300

    function

---
+++ Day_31.py
@@ -0,0 +1,29 @@
+# Ex1
+def my_function():
+	print("Hello in my function")
+
+
+my_function()
+print("-------------------------")
+
+
+# Ex2
+def my_fun(fname):
+	print(fname + " Refsnes")
+
+
+my_fun("Email")
+my_fun("Tobies")
+my_fun("linus")
+print("-------------------------")
+
+
+# Ex3
+def fun_country(country = "Norway"):
+	print("I am from " + country)
+
+
+fun_country("Yemen")
+fun_country("Swedan")
+fun_country()
+print("-------------------------")
