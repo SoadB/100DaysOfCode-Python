04e32c87c26ca1ef83cb73a36a38df0bbe644d10
SoadB <soadb321@gmail.com>
2019-09-25 00:33:13 +0300

    lambda

---
+++ Day_35.py
@@ -0,0 +1,14 @@
+
+# Ex1
+x = lambda a: a + 10
+print(x(5))
+print("-------------------------")
+
+# Ex2
+x = lambda a, b: a * b
+print(x(3, 6))
+print("-------------------------")
+
+x = lambda a, b, c: a + b + c
+print(x(1, 3, 9))
+print("-------------------------")
