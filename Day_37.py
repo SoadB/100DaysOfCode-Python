0b3156abc937eb8a62c73513b64ede362a1dd540
SoadB <soadb321@gmail.com>
2019-09-26 23:16:19 +0300

    Array

---
+++ Day_37.py
@@ -0,0 +1,14 @@
+
+# Ex1
+cars = ['Ford', 'Volvo', 'BMW']
+x = cars[0]
+print(x)
+print("-------------------------")
+
+cars[0] = 'Toyota'
+print(cars)
+print("-------------------------")
+
+x = len(cars)
+print(x)
+print("-------------------------")
