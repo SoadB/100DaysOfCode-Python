6cd0defd571162dfefb42480ba18444dd71954fe
SoadB <soadb321@gmail.com>
2019-10-02 15:01:59 +0300

    Class2

---
+++ Day_42.py
@@ -0,0 +1,24 @@
+
+# Ex1
+class Person:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+	def myfun(self):
+		print("Hello my name is: " + self.name)
+
+
+p1 = Person("John", 36)
+p1.myfun()
+print("-------------------------")
+
+p1.age = 40
+print(p1.age)
+print("-------------------------")
+
+del p1.age
+print(p1.age)
+print("-------------------------")
+
+del p1
+print(p1)
