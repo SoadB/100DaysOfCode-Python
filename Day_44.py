03dd284b72cb6292783eb558c227e506c069c5a0
SoadB <soadb321@gmail.com>
2019-10-04 16:49:27 +0300

    Inheritance2

---
+++ Day_44.py
@@ -0,0 +1,28 @@
+
+
+class Person:
+	def __init__(self, fname, lname):
+		self.firstname = fname
+		self.lastname = lname
+
+	def printname(self):
+		print(self.firstname, self.lastname)
+
+
+class Student(Person):
+	def __init__(self, fname, lname, year):
+		super().__init__(fname, lname)
+		self.graduationyear = year
+
+	def welcom(self):
+		print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+
+x = Student('Mike', 'Olsen', 2019)
+x.printname()
+print("-------------------------")
+
+print(x.graduationyear)
+print("-------------------------")
+
+x.welcom()
