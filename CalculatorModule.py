54bf196e7be9bd22c6f78254d0c6b00aa3746f5d
SoadB <soadb321@gmail.com>
2019-10-12 16:07:34 +0300

    Quiz

---
+++ CalculatorModule.py
@@ -0,0 +1,19 @@
+
+def Sum(x, y):
+	sum = x + y
+	print(sum)
+
+
+def Sub(x, y):
+	sub = x - y
+	print(sub)
+
+
+def Multipli(x, y):
+	mul = x * y
+	print(mul)
+
+
+def Division(x, y):
+	div = x / y
+	print(div)
---
+++ Day_53_54.py
@@ -0,0 +1,17 @@
+
+import CalculatorModule
+import datetime
+
+CalculatorModule.Sum(8, 1)
+CalculatorModule.Sub(4, 2)
+CalculatorModule.Multipli(6, 6)
+CalculatorModule.Division(8, 2)
+print("-------------------------")
+
+x = datetime.datetime.now()
+print(x.year)
+print(x)
+print(x.strftime("%B"))
+print(x.strftime("%A"))
+
+print(x + 1)
