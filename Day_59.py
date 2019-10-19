d95a4b1be8fcde3af2d06cac79ecb6ca78f75713
SoadB <soadb321@gmail.com>
2019-10-19 22:34:32 +0300

    RegEx3

---
+++ Day_59.py
@@ -0,0 +1,15 @@
+
+import re
+txt = 'The rain is Spain'
+x = re.sub("\s", "9", txt)
+print(x)
+print("-------------------------")
+
+x = re.sub("\s", "9", txt, 2)
+print(x)
+print("-------------------------")
+
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+print(x.string)
+print(x.group())
