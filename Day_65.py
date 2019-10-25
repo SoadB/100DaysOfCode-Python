20995acb9d72c2585b0a4de93e40d3ca1a44d192
SoadB <soadb321@gmail.com>
2019-10-26 00:16:27 +0300

    Format2

--- Day_65.py
+++ Day_65.py
@@ -13,3 +13,4 @@ itemno = 567
 myorder = 'I want {} pieces of item number {} for {: .2f} dollars'
 print(myorder.format(quantity, itemno, price))
 print("-------------------------")
+
---
+++ Day_66.py
@@ -0,0 +1,16 @@
+
+price = 49
+quantity = 3
+itemno = 567
+myorder = 'I want {0} pieces of item number {1} for {2: .2f} dollars'
+print(myorder.format(quantity, itemno, price))
+print("-------------------------")
+
+age = 36
+name = 'John'
+txt = 'His name is: {1}. {1} is {0} years old.'
+print(txt.format(age, name))
+print("-------------------------")
+
+myorder = 'I have a {carname}, it is a {model}'
+print(myorder.format(carname='Ford', model='Mustang'))
