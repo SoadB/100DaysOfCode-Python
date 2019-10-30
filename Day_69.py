3654a1e723997198f6ccefa34166f748c5f07590
SoadB <soadb321@gmail.com>
2019-10-30 18:51:55 +0300

    Files

---
+++ Day_69.py
@@ -0,0 +1,39 @@
+
+import os
+f = open("demofile.txt", "r")
+# print(f.read())
+print(f.read(5))
+print("-------------------------")
+
+print(f.readline())
+print(f.readline())
+print("-------------------------")
+
+for x in f:
+	print(x)
+print("-------------------------")
+
+f.close()
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+f = open("demofile2.txt", "r")
+print(f.read())
+print("-------------------------")
+
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+f = open("demofile3.txt", "r")
+print(f.read())
+print("-------------------------")
+
+f = open("myfile.txt", "w")
+
+if os.path.exists("demofile.txt"):
+	os.remove("demofile.txt")
+else:
+	print("The file dose not exists")
